9408804012ad0b87b0a8a59dae39dbb365026692
Narendrakumar Sethuraman <snaren0423@gmail.com>
2024-01-17 10:59:13 +1100

    EP02 commit

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client"; 
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I'm an h1 tag"),
@@ -10,12 +13,6 @@ const parent = React.createElement("div", { id: "parent" }, [
 ]);
 
 console.log(parent);
-
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", xyz: "abc" },
-//   "Hello World from React!"
-// );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # React Is Fun Episode-01
+
+# Parcel Episode-02
+- Dev build 😂
+- Local server
+- HMR = Hot Module Replacement 
+- File Watching Algorithm - written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
+- Consistency Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
+
